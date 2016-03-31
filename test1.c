ccf5daf4cc336be7ec1f68030bbf8dcc874b81be
Leon Tan <leon.arian.tan@gmail.com>
2016-04-01 00:46:23 +0200

    Experiments with PSM in pththreads Shrinked Makefiles by making global include (yet some work to be done here).

--- test1.c
+++ test1.c
@@ -41,7 +41,7 @@ int8_t tfunc0 (ptthread_t* self, uint8_t* data, uint16_t dlength) {
     
     if (ledswitch == 1) {
         PORTB ^= (1 << PB5);
-        printf("Setting PB5 to %d\n", (PORTB >> PB5) & 1  );
+        //printf("Setting PB5 to %d\n", (PORTB >> PB5) & 1  );
     }
     
     ptthread_delay(self, 435);
@@ -52,7 +52,7 @@ int8_t tfunc0 (ptthread_t* self, uint8_t* data, uint16_t dlength) {
 int8_t tfunc1 (ptthread_t* self, uint8_t* data, uint16_t dlength) {
     
     PORTB ^= (1 << PB3);
-    printf("Setting PB3 to %d\n", (PORTB >> PB3) & 1  );
+    //printf("Setting PB3 to %d\n", (PORTB >> PB3) & 1  );
     
     ptthread_delay(self, 1106);
     
@@ -81,7 +81,41 @@ int8_t tfunc2 (ptthread_t* self, uint8_t* data, uint16_t dlength) {
     return 0;
 }
 
-ptthread_t thread[3];
+int8_t tfunc3 (ptthread_t* self, uint8_t* data, uint16_t dlength) {
+    
+    static uint16_t duty_circle = 0;
+    static uint8_t direction = 1;
+    
+    //PORTB ^= (1 << PB1);
+    
+    if (direction) {
+        duty_circle += 65335/50;
+    }
+    else {
+        duty_circle -= 65335/50;
+    }
+    
+    if (duty_circle > 65200) {
+        direction = 0;
+        OCR1A = 0xFFFF;
+        ptthread_delay(self, 2000);
+        return 0;
+    }
+    else if (duty_circle < 100){
+        direction = 1;
+    }
+    OCR1A = duty_circle;
+    //OCR1B = duty_circle;
+    
+    
+    
+    ptthread_delay(self, 20);
+    
+    return 0;
+    
+}
+
+ptthread_t thread[4];
 
 
 
@@ -92,6 +126,26 @@ int main (void) {
     DDRB  = 0xFF;
     DDRB &= ~( (1 << PB4) );
     
+    // set up PB1 as PWM output 
+    // set TOP to 16bit
+    ICR1 = 0xFFFF;
+    
+    // PB1 and PB2 is now an output
+    DDRB |= (1 << DDB1)|(1 << DDB2);
+    
+    
+    
+    // set none-inverting mode
+    TCCR1A |= (1 << COM1A1)|(1 << COM1B1);
+    
+    // set Fast PWM mode using ICR1 as TOP
+    TCCR1A |= (1 << WGM11);
+    TCCR1B |= (1 << WGM12)|(1 << WGM13);
+    
+    // START the timer with no prescaler
+    TCCR1B |= (1 << CS10);
+    
+    
     //ptstream_t teststream;
     //ptstream_init(&teststream, 100);
     
@@ -99,8 +153,9 @@ int main (void) {
     ptthread_init(&thread[0], tfunc0, RUNNING, NULL, 0);
     ptthread_init(&thread[1], tfunc1, RUNNING, NULL, 0);
     ptthread_init(&thread[2], tfunc2, RUNNING, NULL, 0);
+    ptthread_init(&thread[3], tfunc3, RUNNING, NULL, 0);
     
-    ptthread_main(thread, 3);
+    ptthread_main(thread, 4);
     
     
     return 0;
